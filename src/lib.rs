4ebac6c9b6f2f416834a698ca3e1ccc667936d10
A4-Tacks <wdsjxhno1001@163.com>
2025-12-05 17:13:42 +0800

    Change whitespaces sep to braces wrap

--- src/lib.rs
+++ src/lib.rs
@@ -241,23 +241,23 @@ pub fn term_expr_inserts(
                         | "WHITESPACE"
                         | "COMMENT"
                         ))?;
-                at!(tail.start(), r#"_track!{{%"{mark}","#);
-                at!(tail.end(), r#"}}"#);
+                at!(tail.start(), r#"{{_track!{{%"{mark}","#);
+                at!(tail.end(), r#"}}}}"#);
             }
             "CLOSURE_EXPR" => {
                 let tail = node.sub().last()?;
-                at!(tail.start(), r#"{{{try_trait}_track!{{%"{mark}","#);
-                at!(tail.end(), r#"}}}}"#);
+                at!(tail.start(), r#"{{{try_trait}{{_track!{{%"{mark}","#);
+                at!(tail.end(), r#"}}}}}}"#);
             }
             "RETURN_EXPR" => {
                 let kw = node.find_children("RETURN_KW")?;
-                at!(kw.end(), r#" _track!(+"{mark}","#);
-                at!(node.end(), r#")"#);
+                at!(kw.end(), r#"{{_track!(+"{mark}","#);
+                at!(node.end(), r#")}}"#);
             }
             "TRY_EXPR" => {
                 let op = node.find_children("QUESTION")?;
-                at!(node.start(), r#" _track!(@"{mark}","#);
-                at!(op.start(), r#")"#);
+                at!(node.start(), r#"{{_track!(@"{mark}","#);
+                at!(op.start(), r#")}}"#);
             }
             _ => {}
         }
@@ -446,12 +446,12 @@ r#"fn foo(n: u8) -> Option<u8> {
         apply_inserts(inserts, &mut s);
         expect![[r#"
             fn foo(n: u8) -> Option<u8> {trait _IsTryOk{fn is_try_ok(&self)->bool;}impl<T,E>_IsTryOk for ::core::result::Result<T,E>{fn is_try_ok(&self)->bool{self.is_ok()}}impl<T>_IsTryOk for ::core::option::Option<T>{fn is_try_ok(&self)->bool{self.is_some()}}macro_rules!_track{(!)=>(());(!$t:tt)=>($t);(@$s:tt,$($e:expr)?)=>({let __val = _track!(!$($e)?);if !_IsTryOk::is_try_ok(&__val){println!("[track] foo tryret{} at {}:{} = {__val:?}",$s,::core::file!().rsplit_once(['/','\\']).map_or(::core::file!(), |x|x.1),::core::line!())}; __val });(+$s:tt,$($e:expr)?)=>({let __val = _track!(!$($e)?);println!("[track] foo return{} at {}:{} = {__val:?}",$s,::core::file!().rsplit_once(['/','\\']).map_or(::core::file!(), |x|x.1),::core::line!()); __val });(%$s:tt,$($e:expr)?)=>({let __val = _track!(!$($e)?);println!("[track] foo endret{} at {}:{} = {__val:?}",$s,::core::file!().rsplit_once(['/','\\']).map_or(::core::file!(), |x|x.1),::core::line!()); __val });(%$s:tt,$e:stmt $(;)?)=>({{$e};let __val = ();println!("[track] foo endret{} at {}:{} = {__val:?}",$s,::core::file!().rsplit_once(['/','\\']).map_or(::core::file!(), |x|x.1),::core::line!()); });}println!("[track] foo enter     at {}:{}",::core::file!().rsplit_once(['/','\\']).map_or(::core::file!(), |x|x.1),::core::line!());
-                let m =  _track!(@"'1 ",n.checked_sub(16))?;
+                let m = {_track!(@"'1 ",n.checked_sub(16))}?;
                 let _ = || 2;
                 if m == 0 {
-                    return _track!(+"'2 ", None);
+                    return{_track!(+"'2 ", None)};
                 }
-                _track!{%"'0 ",Some(m)}
+                {_track!{%"'0 ",Some(m)}}
             }
         "#]].assert_eq(&s);
     }
